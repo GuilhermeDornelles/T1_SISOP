9bb91eaafcc38debbf232a9fe1d27ed404b2f228
Arthur Kunzler <arthur.gabriel.k@gmail.com>
2023-04-02 10:43:57 -0300

    Add return algorithm type from config_parsing fucn

--- src/main.py
+++ src/main.py
@@ -31,11 +31,12 @@ def parse_config_file(config_file : str) -> tuple[list[Process], str]:
 
 def main():
 
-	processes_list, algorithm = parse_config_file("config.json")
-	# processes_list, algorithm  = parse_config_file("config_SJF.json")
+	# processes_list, algorithm = parse_config_file("config.json")
+	processes_list, algorithm  = parse_config_file("config_SJF.json")
 	if not processes_list:
 		print("Unaccepted configurations on config file. Please try again.")
 		exit(1)
+
 	print(f"Algo is {algorithm}")
 	for process in processes_list:
 		parser = Parser(filename=process.pcb.source_file)
