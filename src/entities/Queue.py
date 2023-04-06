794396817e0e5d5d6251fbfdd659707096c57492
Arthur Kunzler <arthur.gabriel.k@gmail.com>
2023-04-05 22:24:19 -0300

    init schedule logic on RR, and switch_process logic too, with the Queues

--- src/entities/Queue.py
+++ src/entities/Queue.py
@@ -5,3 +5,12 @@ class Queue:
     
     def __init__(self):
         self.processes = []
+
+    def isEmpty(self) -> bool:
+        return len(self.processes) == 0
+
+    def push(self, process : Process):
+        self.processes.append(process)
+
+    def pop(self) -> Process:
+        return self.processes.pop(0)
--- src/entities/RrScheduler.py
+++ src/entities/RrScheduler.py
@@ -1,21 +1,50 @@
 from Scheduler import Scheduler
 from entities.Queue import Queue
+from entities.Process import Process
+from entities.ProcessRR import ProcessRR
+from entities.enums.Priorities import Priorities
 
 class RRScheduler(Scheduler):
-    ready_queue : dict[str, Queue]
+    # Cada uma dessas é a fila de prontos separada pro prioridade; quem vai gerenciar elas vai ser o escalonador dai
+    HP_ready_queue : Queue #HighPriority
+    MP_ready_queue : Queue #MediumPriority
+    LP_ready_queue : Queue #LowPriority
     blocked_queue : Queue
+    running_p : Process
+    exit_list : list[ProcessRR]
     
-
     def __init__(self):
         super.__init__()
         
+    def schedule(self, processes : list[ProcessRR]):
+        for process in processes:
+            if process.priority == Priorities.HIGH_PRIORITY:
+                self.HP_ready_queue.push(process)
+            elif process.priority == Priorities.MEDIUM_PRIORITY:
+                self.MP_ready_queue.push(process)
+            elif process.prioriry == Priorities.LOW_PRIORITY:
+                self.LP_ready_queue.push(process)
+
+        running_p = self.switch_processes()
+        # TODO definir como vamos executar uma instrução desse P
+
+
     def sortByPriority():
         # TODO
         pass
     
-    def switch_processes():
-        pass
-        # TODO
-        # Quando um processo com prioridade mais alta quer executar
+    def switch_processes(self) -> ProcessRR:
+        # Retorna o proximo processo que deve executar
+        if not self.HP_ready_queue.isEmpty():
+            return self.HP_ready_queue.pop()
+        elif not self.MP_ready_queue.isEmpty():
+            return self.MP_ready_queue.pop()
+        elif not self.LP_ready_queue.isEmpty():
+            return self.LP_ready_queue.pop()
+        else:
+            # Se chegarmos aqui é pq nao existe nenhum P pronto
+            # TODO definir o que acontece nesse caso
+            return None
+        
     
     
