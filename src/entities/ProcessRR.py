644f2499b8466b25efa08e4938b9e203728c38d5
Guilherme Dornelles <guilherme.dornelles@nubo.ai>
2023-04-08 14:22:02 -0300

    creating function in processRR that maps priority as a num

--- src/entities/ProcessRR.py
+++ src/entities/ProcessRR.py
@@ -1,6 +1,6 @@
 
-from entities.Process import Process
-from entities.enums.Priorities import Priorities
+from Process import Process
+from enums.Priorities import Priorities
 
 
 class ProcessRR(Process):
@@ -11,6 +11,16 @@ class ProcessRR(Process):
         super().__init__(pid=pid, arrival_time=arrival_time, source_file=source_file)
         self.priority = Priorities(priority)
         self.quantum = quantum
+    
+    def get_priority_as_num(self):
+        match self.priority:
+            case Priorities.HIGH_PRIORITY:
+                return 2
+            case Priorities.MEDIUM_PRIORITY:
+                return 1
+            case Priorities.LOW_PRIORITY:
+                return 0
+        return 0
     
     def __str__(self):
         return super().__str__() + f", priority={self.priority}, quantum={self.quantum}"
