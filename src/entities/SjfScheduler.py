ad7e4a70352cd7dde16cff183db6fc91442e76a6
giovanebmilani <giovanebmilani@hotmail.com>
2023-04-16 12:30:18 -0300

    SJF bombando (precisa de mais testes)

--- src/entities/SjfScheduler.py
+++ src/entities/SjfScheduler.py
@@ -1,6 +1,7 @@
+import random
 from typing import List
 
-from Scheduler import Scheduler
+from .Scheduler import Scheduler
 from .ProcessSJF import ProcessSJF
 from .Context import Context
 from .enums.States import States
@@ -9,7 +10,7 @@ from .enums.ReturnCode import ReturnCode
 class SJFScheduler(Scheduler):
  
     def __init__(self, processes: List[ProcessSJF]):
-        super.__init__(processes=processes)
+        super().__init__(processes=processes)
         self.running_p: ProcessSJF = None
         self.ready_queue: List[ProcessSJF] = []
         self.blocked_queue: List[ProcessSJF] = []
@@ -23,64 +24,98 @@ class SJFScheduler(Scheduler):
                 return True
         return False
 
+    def increment_clock(self):
+        res = super().increment_clock()
+        print(f'\n## time {self.clock} ##')
+        return res
+
     def schedule(self):
         acc = 0
         pc = None
+        is_swaping_context = False
 
         while not self.can_schedule_end:
             # TROCA DE CONTEXTO DE PROCESSO
-            self.increment_clock()
+            if not is_swaping_context:
+                self.increment_clock()
+            is_swaping_context = False
 
+            self._update_blocked_queue()
             self._update_ready_queue()
 
-            self.running_p = self.ready_queue.pop(0)
-            
-            acc = self.running_p.pcb.acc
-            pc = self.running_p.pcb.pc
-            data = self.running_p.pcb.program.data.copy()
-            flags = self.running_p.pcb.program.flags
-
-            current_processing_time = 0
-
-            # EXECUÇÃO DO PROCESSO
-            while not self.exist_process_with_lower_execution_time:
+            if len(self.ready_queue) > 0:
+                self.running_p = self.ready_queue.pop(0)
+                print(f'(scheduled process) pid={self.running_p.pcb.pid}')
                 
-                context = Context(
-                    pc=pc,
-                    acc=acc,
-                    data=data,
-                    flags=flags
-                )
-                result = pc.function(context)
+                acc = self.running_p.pcb.acc
+                pc = self.running_p.pcb.pc
+                data = self.running_p.pcb.program.data.copy()
+                flags = self.running_p.pcb.program.flags
 
-                acc = context.acc
-                pc = context.pc
+                current_processing_time = 0
 
-                self.increment_clock()
-                current_processing_time+=1
-                self._update_ready_queue()
+                # EXECUÇÃO DO PROCESSO
+                while not self.exist_process_with_lower_execution_time:
+                    print(f'(acc={acc}) executing: {pc}')
 
-                if result is not None:
-                    if result == ReturnCode.EXIT:
-                        self.exit_list.append(self.running_p)
-                    if result in [ReturnCode.INPUT, ReturnCode.OUTPUT]:
+                    context = Context(
+                        pc=pc,
+                        acc=acc,
+                        data=data,
+                        flags=flags
+                    )
+                    result = pc.function(context)
+
+                    acc = context.acc
+                    pc = context.pc
+
+                    self.increment_clock()
+                    current_processing_time+=1
+                    self._update_blocked_queue()
+                    self._update_ready_queue()
+
+                    if result is not None:
                         break
-                
-            # Bloqueia processo
-            self.running_p.pcb.state = States.BLOCKED
-            self.blocked_queue.append(self.running_p)
+                    result = None
+                        
+                if result == ReturnCode.EXIT:
+                    print(f'(exiting process) pid={self.running_p.pcb.pid}')
+                    self.exit_list.append(self.running_p)
+                if result in [ReturnCode.INPUT, ReturnCode.OUTPUT]:
+                    # Bloqueia processo
+                    print(f'(blocking process) pid={self.running_p.pcb.pid}')
+                    time_to_wait = random.randint(8, 10)
+                    self.running_p.pcb.state = States.BLOCKED
+                    self.blocked_queue.append(self.running_p)
+                    self.running_p.pcb.time_to_wait = time_to_wait
+                if result is None:
+                    self.running_p.pcb.state = States.READY
+                    self.ready_queue.append(self.running_p)
+                    
+                # Atualiza contexto de execução na PCB
+                self.running_p.pcb.acc = acc
+                self.running_p.pcb.pc = pc
+                self.running_p.pcb.program.data = data
 
-            # Atualiza contexto de execução na PCB
-            self.running_p.pcb.acc = acc
-            self.running_p.pcb.acc = pc
-            self.running_p.pcb.program.data = data
+                is_swaping_context = True
+                self.running_p = None
+            else:
+                print(f'nothing to run.')
 
-            self.running_p = None
+    def _update_blocked_queue(self):
+        for process in self.blocked_queue:
+            if process.pcb.time_to_wait > 0:
+                process.pcb.decrease_time_to_wait()
+            else:
+                print(f'(process gone ready) pid={process.pcb.pid}')
+                process.pcb.state = States.READY
+                self.blocked_queue.remove(process)
+                self.ready_queue.append(process)
 
-            
     def _update_ready_queue(self):
         arriving_processes = self.get_arriving_processes()
         for process in arriving_processes:
+            print(f'(process arrived) pid={process.pcb.pid}')
             process.pcb.init_process(self.clock)
             self.ready_queue.append(process)
 
--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 import os
 from parser.parser import Parser
 from entities.RrScheduler import RRScheduler
+from entities.SjfScheduler import SJFScheduler
 from utils.config_parser import parse_config_file
 
 def main():
-	# config_file = "config_SJF.json"
-	config_file = "config.json"
+	config_file = "config_SJF.json"
+	#config_file = "config.json"
 	script_path = os.path.abspath(__file__)
 	file_path = os.path.join(os.path.dirname(script_path), config_file)
 
@@ -16,8 +17,11 @@ def main():
 
 	print(f"Algo is {algorithm}")
 
-	escalonador_teste = RRScheduler(processes_list)
+	escalonador_teste = SJFScheduler(processes_list)
 	escalonador_teste.schedule()
+
+	# escalonador_teste = RRScheduler(processes_list)
+	# escalonador_teste.schedule()
 
 
 if __name__ == "__main__":
